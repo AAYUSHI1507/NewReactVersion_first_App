4fb4748fc93ff5ec3e915ee489c038215d31e8ea
AAYUSHI1507 <a.kataria@somaiya.edu>
2024-11-21 04:31:21 +0530

    Ok

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,21 @@ import Card from './Components/Card'
 function App() {
   const [count, setCount] = useState(0)
   const [color, setcolor] = useState(0);
+  
   useEffect(() => {
-    alert("Hi !!! Just need a check of start page")
+    alert("Hi !!! Just need a check of start page");
   }, []);
-
   useEffect(() => {
     alert(`Change the color : ${color}`)
     setcolor(color + 1);
   }, [count]);
 
-  useEffect(() => {
-    alert("I am running because the color changed")
-  }, [color]);
-
-  useEffect(() => {
-    alert("This is the first time rendering in App.jsx. Welcome to my page");
-    
-    return () =>{
-      alert("The Component is unmounted");
-    }
-  }, []);
   const HandleUpdateCount = () =>{ 
     setCount(count+1);
   }
   return (
     <>
-      <Navbar color = {"Blue Navy"+color}/>
+      <Navbar count = {count} color = {"Blue Navy"+color}/>
       <div className="cards">
         <Card title = "card 1" description = "card 2 desc" />
         <Card title = "card 2" description = "card 3 desc" />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React,{useEffect} from "react";
 // import Navbar from "Components";
-import './Navbar.css' ;
+import "./Navbar.css";
 
 const Navbar = (props) => {
+  
+  useEffect(() => {
+    alert("I am running because the color changed");
+  }, [props.color]);
+
+  useEffect(() => {
+    alert("This is the first time rendering in App.jsx. Welcome to my page")
+
+    return () => {
+      alert("The Component is unmounted")
+    }
+  }, []);
   return (
     <>
-    <div>
+      <div>
         <nav>
-            <ul>
-                <li>Home {props.color}</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+          <ul>
+            <li>Home {props.color}</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
         </nav>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
